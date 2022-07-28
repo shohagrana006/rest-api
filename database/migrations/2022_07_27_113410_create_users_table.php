1ba89ffc83af3b1690589c3c154a2019b05262e0
Shohag Rana <shohagrana006@gmail.com>
2022-07-28 22:23:39 +0600

    user migration and model edit

--- database/migrations/2022_07_27_113410_create_users_table.php
+++ database/migrations/2022_07_27_113410_create_users_table.php
@@ -15,9 +15,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('fullname', 64);
             $table->string('username', 32)->unique();
             $table->string('email', 64)->unique();
             $table->string('password', 128);
+            $table->tinyInteger('status')->default(0);
             $table->timestamps();
         });
     }
